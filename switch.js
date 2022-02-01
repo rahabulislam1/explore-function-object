66042062671bca0a38ed5ef13ce9ec0d7fb878e6
rahabulislam1 <rahabulislam1@gmail.com>
2022-02-01 20:55:38 +0600

    switch updated

---
+++ switch.js
@@ -0,0 +1,21 @@
+var color = "black";
+
+switch (color) {
+    case 'red':
+        console.log("Color is Red");
+        break;
+    case 'yellow':
+        console.log("Color is yellow");
+        break;
+    case 'blue':
+        console.log("Color is blue");
+        break;
+    case 'brown':
+        console.log("Color is brown");
+        break;
+    case 'black':
+        console.log("Color is black");
+        break;
+    default:
+        console.log("Color is white");
+}
