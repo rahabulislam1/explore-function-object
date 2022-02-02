fb81dad0afbb479d99c2922b43f268d8d817a983
rahabulislam1 <rahabulislam1@gmail.com>
2022-02-02 10:42:00 +0600

    Case conversion,String Sum, Namota problem solve

---
+++ md-19-problem.js
@@ -0,0 +1,28 @@
+// problem-1 Namota
+
+// function namota(nums) {
+//     for (var i = 1; i <= nums; i++) {
+//         console.log('13 * ' + i + ' = ' + (13 * i));
+//     }
+// }
+// namota(13);
+
+//UpperCase to Lowercase
+
+// function caseConvert(massage) {
+//     var text = massage.toLowerCase();
+//     return text;
+// }
+
+// var result = caseConvert("RAHAbul");
+// console.log(result);
+
+//String Sum
+
+function fullName(firstName, lastName) {
+    var result = firstName + ' ' + lastName;
+    return result;
+}
+
+var yourName = fullName('Rahabul', 'Islam');
+console.log(yourName);
